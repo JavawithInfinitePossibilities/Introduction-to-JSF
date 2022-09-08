89aaa7bd664b4d5454cab5522ca0cd21c8640184
Lenovo <Lenovo@192.168.1.11>
2022-09-08 18:20:55 +0530

    Section-03-Form-Validation-Under-JSF

---
+++ Learn-JSF-from-basic-to-database-connectivity-quickly/Section-03-Form-Validation-Under-JSF/src/main/java/com/sid/jsf/tutorial/Section03FormValidationUnderJSF.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sid.jsf.tutorial;
+
+/**
+ * @author Lenovo
+ *
+ */
+public class Section03FormValidationUnderJSF {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
